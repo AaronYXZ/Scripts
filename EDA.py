75edb15d9aef0b4c5539e866d07a3f7d8322fa76
Aaron Yu <AaronYXZ@users.noreply.github.com>
2017-12-19 16:52:20 -0500

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -4,3 +4,6 @@ corr = df.corr()
 f. ax = plt.subplots(figsize = (15,15))
 cmap = sns.diverging_palette(150, 20, as_cmap=True)
 sns.heatmap(corr, cbar = True, annot = True, square = True, fmt = '.2f',annot_kws={'size': 18}, cmap = cmap)
+
+## set legend location
+plt.legend(loc='lower right') #('upper left')
