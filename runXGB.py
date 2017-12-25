711d4e04c92844a6b48ff8a7d707bfd4d8326418
Aaron Yu <AaronYXZ@users.noreply.github.com>
2017-12-25 17:47:26 -0500

    Create runXGB.py

---
+++ runXGB.py
@@ -0,0 +1,27 @@
+def runXGB(train_X, train_y, test_X, test_y=None, feature_names=None, seed_val=0, num_rounds=1000):
+    param = {}
+    param['objective'] = 'multi:softprob'
+    param['eta'] = 0.03
+    param['max_depth'] = 6
+    param['silent'] = 1
+    param['num_class'] = 3
+    param['eval_metric'] = "mlogloss"
+    param['min_child_weight'] = 1
+    param['subsample'] = 0.7
+    param['colsample_bytree'] = 0.7
+    param['seed'] = seed_val
+    num_rounds = num_rounds
+
+    plst = list(param.items())
+    xgtrain = xgb.DMatrix(train_X, label=train_y)
+
+    if test_y is not None:
+        xgtest = xgb.DMatrix(test_X, label=test_y)
+        watchlist = [ (xgtrain,'train'), (xgtest, 'test') ]
+        model = xgb.train(plst, xgtrain, num_rounds, watchlist, early_stopping_rounds=20)
+    else:
+        xgtest = xgb.DMatrix(test_X)
+        model = xgb.train(plst, xgtrain, num_rounds)
+
+    pred_test_y = model.predict(xgtest)
+    return pred_test_y, model
